f67d89daae8cf532db2d0cef7e6e3bfd85ef8386
= <numenor21@gmail.com>
2018-10-07 17:00:50 -0400

    Add method create() to the TestCase class

--- tests/Feature/UsersModuleTest.php
+++ tests/Feature/UsersModuleTest.php
@@ -2,10 +2,10 @@
 
 namespace Tests\Feature;
 
-use App\{User, Profession};
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\{User, Profession};
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UsersModuleTest extends TestCase
 {
@@ -21,11 +21,43 @@ class UsersModuleTest extends TestCase
     }
 
     /** @test */
+    function it_loads_the_users_list()
+    {
+        $response = $this->get('/users')
+            ->assertStatus(200)
+            ->assertSee('Usuarios');
+    }
+
+    /** @test */
+    function it_loads_the_user_details()
+    {
+        $profession = $this->create(Profession::class, ['title' => 'Desarrollador web']);
+
+        $user = $this->create(User::class, [
+            'name' => 'Cristyan Valera',
+            'email' => 'cristyan12@mail.com',
+            'profession_id' => $profession->id,
+        ]);
+        
+        $response = $this->get(route('users.show', $user))
+            ->assertStatus(200)
+            ->assertSee('Cristyan Valera')
+            ->assertSee('cristyan12@mail.com')
+            ->assertSee('Desarrollador web');
+    }
+
+    /** @test */
+    function it_show_a_default_message_when_list_of_user_is_empty()
+    {   
+        $response = $this->get(route('users.index'))
+            ->assertStatus(200)
+            ->assertSee('No hay usuarios registrados');
+    }
+
+    /** @test */
     function it_creates_a_new_user()
     {
-        $this->withoutExceptionHandling();
-
-        $profession = factory(Profession::class)->create([
+        $profession = $this->create(Profession::class, [
             'title' => 'Desarrollador web'
         ]);
 
@@ -47,7 +79,7 @@ class UsersModuleTest extends TestCase
     /** @test */
     function the_name_field_is_required()
     {
-        $profession = factory(Profession::class)->create();
+        $profession = $this->create(Profession::class);
 
         $this->from('users')
             ->post(route('users.store'), [
@@ -65,7 +97,7 @@ class UsersModuleTest extends TestCase
     /** @test */
     function the_email_field_is_required()
     {
-        $profession = factory(Profession::class)->create();
+        $profession = $this->create(Profession::class);
 
         $this->from('users')
             ->post(route('users.store'), [
@@ -83,11 +115,9 @@ class UsersModuleTest extends TestCase
     /** @test */
     function the_email_field_must_be_unique()
     {        
-        factory(User::class)->create([
-            'email' => 'cristyan@mail.com',
-        ]);
+        $this->create(User::class, ['email' => 'cristyan@mail.com']);
 
-        $profession = factory(Profession::class)->create();
+        $profession = $this->create(Profession::class);
 
         $this->from('users')
             ->post(route('users.store'), [
@@ -105,13 +135,9 @@ class UsersModuleTest extends TestCase
     /** @test */
     function it_can_load_the_form_to_update_user()
     {
-        $this->withoutExceptionHandling();
+        $profession = $this->create(Profession::class, ['title' => 'Carpintero']);
 
-        $profession = factory(Profession::class)->create([
-            'title' => 'Carpintero'
-        ]);
-
-        $user = factory(User::class)->create([
+        $user = $this->create(User::class, [
             'name' => 'Cristyan',
             'email' => 'cristyan@mail.com',
             'profession_id' => $profession->id
@@ -124,45 +150,5 @@ class UsersModuleTest extends TestCase
             ->assertSee('Cristyan')
             ->assertSee('cristyan@mail.com')
             ->assertSee('Carpintero');
-    }
-
-    /** @test */
-    function it_loads_the_users_list()
-    {
-        $response = $this->get('/users')
-            ->assertStatus(200)
-            ->assertSee('Usuarios');
-    }
-
-    /** @test */
-    function it_loads_the_user_details()
-    {
-        $this->withoutExceptionHandling();
-
-        $profession = factory(Profession::class)->create([
-            'title' => 'Desarrollador web'
-        ]);
-
-        $user = factory(User::class)->create([
-            'name' => 'Cristyan Valera',
-            'email' => 'cristyan12@mail.com',
-            'profession_id' => $profession->id,
-        ]);
-        
-        $response = $this->get(route('users.show', $user))
-            ->assertStatus(200)
-            ->assertSee('Cristyan Valera')
-            ->assertSee('cristyan12@mail.com')
-            ->assertSee('Desarrollador web');
-    }
-
-    /** @test */
-    function it_show_a_default_message_when_list_of_user_is_empty()
-    {
-        $this->withoutExceptionHandling();
-        
-        $response = $this->get(route('users.index'))
-            ->assertStatus(200)
-            ->assertSee('No hay usuarios registrados');
     }
 }
