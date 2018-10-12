a907fd37be7984e3b8107edf405bc7d749a96840
= <numenor21@gmail.com>
2018-10-12 15:00:28 -0400

    improvement some fix in the layout

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,12 +4,8 @@
 
 @section('content')
 
-@if($users->count() == 0)
-<h1 class="display-4">No hay usuarios registrados</h1>
-@else
-
 <div class="d-flex justify-content-between align-items-end mb-3">
-    <h1 class="pb-1 display-4">Lista de usuarios</h1>
+        <h1 class="pb-1 display-4">Lista de usuarios</h1>
     <p>
         <a href="{{ route('users.create') }}" class="btn btn-outline-primary">
             Nuevo usuario
@@ -17,36 +13,39 @@
     </p>
 </div>
 
-<table class="table table-borderless table-striped table-hover">
-    <thead class="bg-dark text-white">
-        <th>ID</th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Last updated</th>
-        <th colspan="3">
-            &nbsp;
-        </th>
-    </thead>
-    <tbody>
-        @forelse($users as $user)
-        <tr>
-            <td width="10px">{{ $user->id }}</td>
-            <td>{{ $user->name }}</td>
-            <td>{{ $user->email }}</td>
-            <td>{{ $user->updated_at->diffForHumans() }}</td>
-            <td width="10px">
-                <a href="{{ route('users.show', $user->id) }}" class="btn btn-outline-info btn-sm">Ver</a>
-            </td>
-            <td width="10px">
-                <a href="{{ route('users.edit', $user) }}" class="btn btn-outline-warning btn-sm">Editar</a>
-            </td>
-            <td width="10px">
-                <a href="#" class="btn btn-outline-danger btn-sm">Eliminar</a>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+@if(! $users->count() == 0)
+    <table class="table table-borderless table-striped table-hover">
+        <thead class="bg-dark text-white">
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Last updated</th>
+            <th colspan="3">
+                &nbsp;
+            </th>
+        </thead>
+        <tbody>
+            @forelse($users as $user)
+            <tr>
+                <td width="10px">{{ $user->id }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>{{ $user->updated_at->diffForHumans() }}</td>
+                <td width="10px">
+                    <a href="{{ route('users.show', $user->id) }}" class="btn btn-outline-info btn-sm">Ver</a>
+                </td>
+                <td width="10px">
+                    <a href="{{ route('users.edit', $user) }}" class="btn btn-outline-warning btn-sm">Editar</a>
+                </td>
+                <td width="10px">
+                    <a href="#" class="btn btn-outline-danger btn-sm">Eliminar</a>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+@else
+    <p class="lead">No hay usuarios registrados.</p>
 @endif
 
 {{ $users->render() }}
